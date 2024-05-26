6e3994c241673764a7b185b4b5c2e73ce966f2d7
GustavoNMendes01 <gustavo.nmendes1@gmail.com>
2024-05-26 13:12:43 -0300

    Delete concatena.js

--- typescript/op2/concatena.js
+++
@@ -1,6 +0,0 @@
-    function concatena(str1, str2) {
-        var result = '';
-        // Adiciona os caracteres da primeira string
-        for (var i = 0; i < str1.length; i++) {
-            result += str1[i];
-        }
