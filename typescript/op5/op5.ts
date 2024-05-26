63f72e965bbd2f9c2de9d259c237559d88746b12
GustavoNMendes01 <gustavo.nmendes1@gmail.com>
2024-05-26 13:13:22 -0300

    Create op5.ts

---
+++ typescript/op5/op5.ts
@@ -0,0 +1,34 @@
+function op555(id: number){
+    //INÍCIO - PARTE PADRÃO
+    var myWindow = window.open('', 'Calculadora Científica', 'toolbar=0,location=0,menubar=0,width=500,height=300');
+    var nomeOp = document.getElementsByTagName('a')[id].innerHTML;
+    myWindow?.document.write(`<html><head><title>${nomeOp}</title></head></html>`);
+
+function concatena(str1: string, str2: string): string {
+    let result = '';
+    
+    // Adiciona os caracteres da primeira string
+    for (let i = 0; i < str1.length; i++) {
+        result += str1[i];
+    }
+
+    // Adiciona os caracteres da segunda string
+    for (let i = 0; i < str2.length; i++) {
+        result += str2[i];
+    }
+
+    return result;
+}
+
+// Obtendo as entradas do usuário
+const string1 = prompt("Digite o primeiro : ");
+const string2 = prompt("Digite o segundo : ");
+
+// Certifique-se de que as entradas não são nulas
+if (string1 !== null && string2 !== null) {
+    const aux = concatena(string1, string2);
+    console.log(aux);  // Saída: a concatenação das duas strings
+} else {
+    console.log("Entradas inválidas.");
+}
+}
