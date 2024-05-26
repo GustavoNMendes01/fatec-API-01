70065dd3fd7386e206c250a9ebc9c2c580918495
GustavoNMendes01 <gustavo.nmendes1@gmail.com>
2024-05-26 13:13:35 -0300

    Update script.js

--- typescript/script.js
+++ typescript/script.js
@@ -49,6 +49,39 @@ function op1(id) {
             break;
     }
 }
+function op2(id) {
+    //INÍCIO - PARTE PADRÃO
+    var myWindow = window.open('', 'Calculadora Científica', 'toolbar=0,location=0,menubar=0,width=500,height=300');
+    var nomeOp = document.getElementsByTagName('a')[id].innerHTML;
+    myWindow === null || myWindow === void 0 ? void 0 : myWindow.document.write(`<html><head><title>${nomeOp}</title></head></html>`);
+    function leia(texto) {
+        return (myWindow === null || myWindow === void 0 ? void 0 : myWindow.prompt(texto));
+    }
+    function escreva(texto) {
+        return (myWindow === null || myWindow === void 0 ? void 0 : myWindow.alert(texto));
+    }
+    function fechar() {
+        return myWindow === null || myWindow === void 0 ? void 0 : myWindow.close();
+    }
+        var numero = Number(leia("Insira um número"));
+    
+        if (numero <= 0) {
+            escreva("Insira um número maior que zero.");
+            fechar();
+        } else {
+            var resultado = 1;
+            for (var i = 1; i <= numero; i++) {
+                resultado *= i;
+            }
+            escreva("O fatorial é " + resultado);
+            fechar();
+        }
+    }   
+    
+       
+
+
+
 
 
 function op4(id) {
